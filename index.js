44b00c514e33ab8270b63642320ddbbe41f33450
ninsid711 <siddy0041@gmail.com>
2024-03-14 22:58:42 +0530

    learnt some basics of js, and it's fire ���

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log(`Hello boi`);
+// console.log(`My balls hurt`);
+
+// window.alert(`Alert`)
+// window.alert(`Alert`)
+// window.alert(`Alert`)
+
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `lorem ipsum`
+
+// let x;
+// let y;
+// y = 1;
+// x = 2;
+// console.log(x+y);
